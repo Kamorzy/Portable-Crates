0a22bcf03df9a7c58243d4b303d5ed22b73b7a28
Kamorzy <kamorzy@gmail.com>
2026-03-28 14:15:07 +0200

    Rewrite for latest MC version from Fabric template mod

---
+++ src/client/java/kamorzy/portablecrates/PortableCratesClient.java
@@ -0,0 +1,10 @@
+package kamorzy.portablecrates;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PortableCratesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
